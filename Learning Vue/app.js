2b28f0b16c0302b8fb3f7b12a3e5392cb6f365f6
Alfie Phillips <17173@st-ives.cornwall.sch.uk>
2024-04-23 21:29:14 +0100

    Linked CDN and mounting vue components :)

--- Learning Vue/app.js
+++ Learning Vue/app.js
@@ -1,1 +1,8 @@
-console.log("Hello Vue!")
+// Create Vue App instance
+const app = Vue.createApp({
+    // Data, Functions
+    template: "<h2>I am the template</h2>"
+})
+
+// Mount Vue component to an id of #app
+app.mount("#app")
