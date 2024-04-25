2b0de816daaaf6dad3bd2ec9645429db3f0dca2a
Alfie Phillips <17173@st-ives.cornwall.sch.uk>
2024-04-25 23:31:54 +0100

    learning: loading in dynamic data using data functions.

--- Learning Vue/app.js
+++ Learning Vue/app.js
@@ -2,6 +2,14 @@
 const app = Vue.createApp({
     // You can either create the template during intialization or create it directly in the app. This is where dynamic data is defined to be accessed in html.
     
+    data() { // The data function; serve data into the application.
+        return {
+            // Return an object of listed variables to use.
+            title: "The Final Empire",
+            author: "Alfie Phillips",
+            age: Math.random().toFixed(2) * 100
+        }
+    }
 })
 
 // Mount Vue component to an id of #app
